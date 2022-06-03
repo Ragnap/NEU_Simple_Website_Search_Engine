fd9c3b7db18f99709466c706387d0fe059a55b45
Ragnap <33452496+Ragnap@users.noreply.github.com>
2022-06-03 10:50:51 +0800

    完成中文临时索引
    生成完成了单词编号和网页编号以及临时索引

--- wordSegmetation/wordSegmentation.cpp
+++ wordSegmetation/wordSegmentation.cpp
@@ -4,14 +4,21 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-const char* const DICT_PATH = "./algorithm_class/wordSegmetation/cppjieba/dict/jieba.dict.utf8";  //最大概率法分词所使用的词典路径
-const char* const HMM_PATH = "./algorithm_class/wordSegmetation/cppjieba/dict/hmm_model.utf8";  //隐式马尔科夫模型分词所使用的词典路径
-const char* const USER_DICT_PATH = "./algorithm_class/wordSegmetation/cppjieba/dict/user.dict.utf8";  //用户自定义词典路径
-const char* const IDF_PATH = "./algorithm_class/wordSegmetation/cppjieba/dict/idf.utf8";  // IDF路径
-const char* const STOP_WORD_PATH = "./algorithm_class/wordSegmetation/cppjieba/dict/stop_words.utf8";  //停用词路径
-const char* const REMOVE_WORD_PATH = "./algorithm_class/wordSegmetation/cppjieba/dict/REMOVE_words.utf8";  //无意义词路径
-const char* const TEXT_PATH = "./algorithm_class/wordSegmetation/cppjieba/dict/REMOVE_words.utf8";  //文本路径
+// jieba词典路径
+const char* const DICT_PATH = "./simple_website_search_engine/wordSegmetation/cppjieba/dict/jieba.dict.utf8";  //最大概率法分词所使用的词典路径
+const char* const HMM_PATH = "./simple_website_search_engine/wordSegmetation/cppjieba/dict/hmm_model.utf8";  //隐式马尔科夫模型分词所使用的词典路径
+const char* const USER_DICT_PATH = "./simple_website_search_engine/wordSegmetation/cppjieba/dict/user.dict.utf8";  //用户自定义词典路径
+const char* const IDF_PATH = "./simple_website_search_engine/wordSegmetation/cppjieba/dict/idf.utf8";  // IDF路径
+const char* const STOP_WORD_PATH = "./simple_website_search_engine/wordSegmetation/cppjieba/dict/stop_words.utf8";  //停用词路径
+const char* const REMOVE_WORD_PATH = "./simple_website_search_engine/wordSegmetation/cppjieba/dict/REMOVE_words.utf8";  //无意义词路径
+//数据文件储存路径
+const char* const TEXT_PATH = "./simple_website_search_engine/websiteText.txt";  //文本路径
+const char* const OUTPUT_TEMP_TEXT_PATH = "./simple_website_search_engine/tempIndex/tempIndex";  //临时索引文件夹路径
+const char* const WORD_MAP_PATH = "./simple_website_search_engine/mapWord.txt";  //单词编号文件
+const char* const WEBSITE_MAP_PATH = "./simple_website_search_engine/mapWebsite.txt";  //网站编号文件
 #define REMOVE_MEANLESS_WORD 1  //当值为1时移除无意义词
+#define PAGE_PER_FILE 50  //一个临时索引文件中网页的数量
+//基于jieba的中文分词器
 class WordSegmentation {
 public:
     WordSegmentation()
@@ -47,7 +54,8 @@ private:
     cppjieba::Jieba tool;  // jieba分词类
     map<string, bool> removeWord;  //无意义词表
 } wordSeg;
-class WordMap {  //储存单词和单词编号的映射
+//储存单词和单词编号的映射
+class WordMap {
 public:
     WordMap() {
         wordCnt = 0;
@@ -71,11 +79,46 @@ public:
         else
             return ID;
     }
+    //输出到文件
+    void saveToFile() {
+        ofstream outputFile(WORD_MAP_PATH);
+        outputFile << wordCnt << endl;
+        for(int i = 0; i < wordCnt; i++) {
+            outputFile << i + 1 << " " << map[i] << endl;
+        }
+        outputFile.close();
+    }
 
 private:
     int wordCnt;
     vector<string> map;
 } wordMap;
+//储存网站和网站编号的映射
+class WebsiteMap {
+public:
+    WebsiteMap() {
+        websiteCnt = 0;
+        map.clear();
+    }
+    //在尾部增加一个网站
+    void push_back(string word) {
+        websiteCnt++;
+        map.push_back(word);
+    }
+    //输出到文件
+    void saveToFile() {
+        ofstream outputFile(WEBSITE_MAP_PATH);
+        outputFile << websiteCnt << endl;
+        for(int i = 0; i < websiteCnt; i++) {
+            outputFile << i + 1 << " " << map[i] << endl;
+        }
+        outputFile.close();
+    }
+
+private:
+    int websiteCnt;
+    vector<string> map;
+} websiteMap;
 //去掉收尾空格
 string& clearHeadTailSpace(string& str) {
     if(str.empty()) {
@@ -85,56 +128,61 @@ string& clearHeadTailSpace(string& str) {
     str.erase(str.find_last_not_of(" ") + 1);
     return str;
 }
-//读取一行内容
-string getStringWord() {
-    string word;
-    getline(cin, word);
-    clearHeadTailSpace(word);
-    return word;
-}
 
 int main() {
     freopen("./Input_data/data.in", "r", stdin);
     freopen("./Output_data/data.out", "w", stdout);
 
-    string id, herf, title, word;
-    // cin >> id;
-    // vector<string> res = wordSeg.segmentation(id);
-    // for(auto i: res) {
-    //     cout << i << endl;
-    // }
-    int cnt = 0;
+    int pageID = 0;
+    ifstream inText(TEXT_PATH);
+    ofstream outTempIndex;
     while(1) {
-        cout << endl << "##############" << endl;
-        getline(cin, id);
+        string id, herf, title, word;
+        //分词开始
+        getline(inText, id);
         if(id.empty())
             break;
-        getline(cin, herf);
-        cout << id << " " << herf << endl;
-        cout << "# ------------" << endl;
 
-        getline(cin, title);
-        cout << "title:" << endl << " -> ";
-        vector<string> res = wordSeg.segmentation(title);
-        for(auto nowWord: res) {
-            cout << nowWord << " ";
+        //读取链接
+        getline(inText, herf);
+        //对应到网站编号
+        if(pageID % PAGE_PER_FILE == 0) {  //每30个网页用一个文件
+            outTempIndex.close();
+            string targetFile = OUTPUT_TEMP_TEXT_PATH + to_string(pageID / PAGE_PER_FILE) + ".txt";
+            cerr << "at file: " << targetFile << endl;
+            outTempIndex.open(targetFile);
         }
-        cout << endl;
+        pageID++;
+        cerr << "now website:" << pageID << endl;
+        websiteMap.push_back(herf);
 
-        word = getStringWord();
+        //处理标题
+        // getline(inText, title);
+        // outTempIndex << "title:" << endl << " -> ";
+        // vector<string> res = wordSeg.segmentation(title);
+        // for(auto nowWord: res) {
+        //     outTempIndex << nowWord << " ";
+        // }
+        // outTempIndex << endl;
+
+        //处理正文
+        getline(inText, word);
+        clearHeadTailSpace(word);
         while(1) {
-            // cout << "#  --" << word << endl;
             if(word == "|")
                 break;
-            res = wordSeg.segmentation(word);
+            vector<string> res = wordSeg.segmentation(word);
             for(auto nowWord: res) {
-                cout << wordMap.getID(nowWord) << ":" << nowWord << endl;
+                outTempIndex << wordMap.getID(nowWord) << "\t" << pageID << "\n";
             }
-            cout << endl;
-            word = getStringWord();
-        }
-        getline(cin, id);
-    }
 
+            getline(inText, word);
+            clearHeadTailSpace(word);
+        }
+        getline(inText, id);
+    }
+    outTempIndex.close();
+    wordMap.saveToFile();
+    websiteMap.saveToFile();
     return 0;
 }
